d01adc00df7e3bf31b99f737fefcb50e295f6cca
Anthony Wang <anthonyindeepspace@gmail.com>
2022-03-23 18:29:07 -0500

    commit 1

---
+++ plot.py
@@ -0,0 +1,34 @@
+# Result Visualization Helper
+from matplotlib import pyplot as plt
+
+M, N = 4, 6
+RESULT_IMG_PATH = '/tmp/test_result.jpg'
+CIFAR10_LABELS = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+                 'dog', 'frog', 'horse', 'ship', 'truck']
+
+def plot_results(images, labels, preds):
+  images, labels, preds = images[:M*N], labels[:M*N], preds[:M*N]
+  inv_norm = transforms.Normalize(
+      mean=(-0.4914/0.2023, -0.4822/0.1994, -0.4465/0.2010),
+      std=(1/0.2023, 1/0.1994, 1/0.2010))
+
+  num_images = images.shape[0]
+  fig, axes = plt.subplots(M, N, figsize=(16, 9))
+  fig.suptitle('Correct / Predicted Labels (Red text for incorrect ones)')
+
+  for i, ax in enumerate(fig.axes):
+    ax.axis('off')
+    if i >= num_images:
+      continue
+    img, label, prediction = images[i], labels[i], preds[i]
+    img = inv_norm(img)
+    img = img.permute(1, 2, 0) # (C, M, N) -> (M, N, C)
+    label, prediction = label.item(), prediction.item()
+    if label == prediction:
+      ax.set_title(u'\u2713', color='blue', fontsize=22)
+    else:
+      ax.set_title(
+          'X {}/{}'.format(CIFAR10_LABELS[label],
+                          CIFAR10_LABELS[prediction]), color='red')
+    ax.imshow(img)
+  plt.savefig(RESULT_IMG_PATH, transparent=True)
---
+++ training.py
@@ -0,0 +1,111 @@
+import resnet
+SERIAL_EXEC = xmp.MpSerialExecutor()
+# Only instantiate model weights once in memory.
+WRAPPED_MODEL = xmp.MpModelWrapper(ResNet152())
+
+def train_resnet152():
+  torch.manual_seed(1)
+
+  def get_dataset():
+    norm = transforms.Normalize(
+        mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010))
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        norm,
+    ])
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        norm,
+    ])
+    train_dataset = datasets.CIFAR10(
+        root=FLAGS['data_dir'],
+        train=True,
+        download=True,
+        transform=transform_train)
+    test_dataset = datasets.CIFAR10(
+        root=FLAGS['data_dir'],
+        train=False,
+        download=True,
+        transform=transform_test)
+    
+    return train_dataset, test_dataset
+  
+  # Using the serial executor avoids multiple processes
+  # to download the same data.
+  train_dataset, test_dataset = SERIAL_EXEC.run(get_dataset)
+
+  train_sampler = torch.utils.data.distributed.DistributedSampler(
+      train_dataset,
+      num_replicas=xm.xrt_world_size(),
+      rank=xm.get_ordinal(),
+      shuffle=True)
+  train_loader = torch.utils.data.DataLoader(
+      train_dataset,
+      batch_size=FLAGS['batch_size'],
+      sampler=train_sampler,
+      num_workers=FLAGS['num_workers'],
+      drop_last=True)
+  test_loader = torch.utils.data.DataLoader(
+      test_dataset,
+      batch_size=FLAGS['batch_size'],
+      shuffle=False,
+      num_workers=FLAGS['num_workers'],
+      drop_last=True)
+
+  # Scale learning rate to num cores
+  learning_rate = FLAGS['learning_rate'] * xm.xrt_world_size()
+
+  # Get loss function, optimizer, and model
+  device = xm.xla_device()
+  model = WRAPPED_MODEL.to(device)
+  optimizer = optim.Adam(params,lr=0.005,betas=(0.9,0.999),eps=1e-08,weight_decay=0,amsgrad=False)
+  loss_fn = nn.NLLLoss()
+
+  def train_loop_fn(loader):
+    tracker = xm.RateTracker()
+    model.train()
+    for x, (data, target) in enumerate(loader):
+      optimizer.zero_grad()
+      output = model(data)
+      loss = loss_fn(output, target)
+      loss.backward()
+      xm.optimizer_step(optimizer)
+      tracker.add(FLAGS['batch_size'])
+      if x % FLAGS['log_steps'] == 0:
+        print('[xla:{}]({}) Loss={:.5f} Rate={:.2f} GlobalRate={:.2f} Time={}'.format(
+            xm.get_ordinal(), x, loss.item(), tracker.rate(),
+            tracker.global_rate(), time.asctime()), flush=True)
+
+  def test_loop_fn(loader):
+    total_samples = 0
+    correct = 0
+    model.eval()
+    data, pred, target = None, None, None
+    for data, target in loader:
+      output = model(data)
+      pred = output.max(1, keepdim=True)[1]
+      correct += pred.eq(target.view_as(pred)).sum().item()
+      total_samples += data.size()[0]
+
+    accuracy = 100.0 * correct / total_samples
+    print('[xla:{}] Accuracy={:.2f}%'.format(
+        xm.get_ordinal(), accuracy), flush=True)
+    return accuracy, data, pred, target
+
+  # Train and eval loops
+  accuracy = 0.0
+  data, pred, target = None, None, None
+  for epoch in range(1, FLAGS['num_epochs'] + 1):
+    para_loader = pl.ParallelLoader(train_loader, [device])
+    train_loop_fn(para_loader.per_device_loader(device))
+    xm.master_print("Finished training epoch {}".format(epoch))
+
+    para_loader = pl.ParallelLoader(test_loader, [device])
+    accuracy, data, pred, target  = test_loop_fn(para_loader.per_device_loader(device))
+    if FLAGS['metrics_debug']:
+      xm.master_print(met.metrics_report(), flush=True)
+
+  return accuracy, data, pred, target
+  
